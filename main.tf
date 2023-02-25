dc2f42c74c68563559abc4a9edf8b98aff2a3bb4
Llewellyn-Liu <liurunlin0213@live.com>
2023-02-25 07:52:29 -0800

    terraform update

---
+++ main.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "3.4.3"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region     = "us-west-2"
+  access_key = "AKIATKDBMDVFOLYDIBXT"
+  secret_key = "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"
+}
+
