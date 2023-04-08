0fe54287319819ce5824cb60cadf243714400beb
Runlin Liu <39145397+Llewellyn-Liu@users.noreply.github.com>
2023-04-08 07:43:54 -0700

    07-final (#4)
    * 05-final
    
    * 05-final
    
    * 05-final
    
    * 05-final
    
    * 05-final
    
    * 05-final
    
    * 07-final

--- liu-station-spring/src/main/java/com/lrl/liustationspring/controller/ProductImageController.java
+++ liu-station-spring/src/main/java/com/lrl/liustationspring/controller/ProductImageController.java
@@ -3,6 +3,8 @@ package com.lrl.liustationspring.controller;
 
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
@@ -39,9 +41,62 @@ public class ProductImageController {
         ImageDataService imageService = ImageDataService.getInstance();
         return imageService.getImagesUsingProductId(id);
     }
-
     @RequestMapping(value = "/data/product/{pathProductId}/image", method = RequestMethod.POST)
+    public String methodDispatcher(HttpServletRequest request, HttpServletResponse response, @PathVariable int pathProductId, @RequestBody String input){
+
+        ObjectMapper mapper = new ObjectMapper();
+        boolean isSingle = true;
+        try{
+            Image i = mapper.readValue(input, Image.class);
+        } catch (JsonMappingException e) {
+            isSingle = false;
+        } catch (JsonProcessingException e) {
+            isSingle = false;
+        }
+
+
+        try{
+            Image[] images = mapper.readValue(input, Image[].class);
+            System.out.println(images[0].getFilename() + ", "+ images[1].getFilename());
+        } catch (JsonMappingException e) {
+            if(!isSingle){
+                logger.info("Neither single image nor multiple. Please check your input");
+                response.setStatus(400);
+                return "";
+            }
+        } catch (JsonProcessingException e) {
+            if(!isSingle){
+                logger.info("Neither single image nor multiple. Please check your input");
+                response.setStatus(400);
+                return "";
+            }
+        }
+
+        if(isSingle) {
+            try {
+                ImageMeta meta = createNewImageForProduct(request, response, pathProductId, mapper.readValue(input, Image.class));
+                return mapper.writeValueAsString(meta);
+            } catch (JsonProcessingException e) {
+                logger.error("This is not supposed to happen. Please check json data format");
+            }
+        }
+        else {
+            try {
+                ImageMeta[] metas = createImagesForProduct(request, response, pathProductId, mapper.readValue(input, Image[].class));
+                return mapper.writeValueAsString(metas);
+            } catch (JsonProcessingException e) {
+                logger.error("This is not supposed to happen. Please check json data format(multi)");
+            }
+        }
+
+        response.setStatus(400);
+        return "";
+    }
+
+
     public ImageMeta createNewImageForProduct(HttpServletRequest request, HttpServletResponse response, @PathVariable int pathProductId, @RequestBody Image img) {
+
+        logger.debug("Enter single image method.");
 
         if (!authentication(request)) {
             response.setStatus(401);
@@ -78,39 +133,96 @@ public class ProductImageController {
 
     }
 
-    @RequestMapping(value = "/data/product/{productId}/image/{imageId}")
-    public Image getImageUsingId(HttpServletRequest request, HttpServletResponse response, @PathVariable int productId, @PathVariable int imageId) {
 
+    public ImageMeta[] createImagesForProduct(HttpServletRequest request, HttpServletResponse response, @PathVariable int pathProductId, @RequestBody Image[] imgs){
+
+        logger.debug("Enter multiple image method.");
         if (!authentication(request)) {
             response.setStatus(401);
-            logger.info("Authentication Failed.");
             return null;
         }
 
-        return null;
+        ObjectMapper mapper = new ObjectMapper();
+
+
+        logger.info("Read from inputStream: " + imgs.toString());
+
+        if (!validateProductId(pathProductId, request)) {
+            response.setStatus(400);
+            logger.info("Validation for productId failed.");
+            return null;
+        }
+
+        ImageMeta[] metas = new ImageMeta[imgs.length];
+
+        for(int i = 0; i< imgs.length; i++){
+            Image img = imgs[i];
+            String imgBase64 = saveImage(img.getFile(), pathProductId, img.getFilename(), img.getType());
+            logger.info("saved path: " + imgBase64);
+            img.setS3BucketPath(imgBase64);
+            img.setProductId(pathProductId);
+            img.setDateCreated(new Date(System.currentTimeMillis()));
+            boolean addSuccess = ImageDataService.getInstance().addImage(img);
+
+            if (!addSuccess) {
+                logger.info("Image cannot be added: " + img.toString());
+                return null;
+            }
+
+            metas[i] = ImageDataService.getInstance().getMetaUsingPath(imgBase64);
+        }
+
+        return metas;
+
+    }
+    @RequestMapping(value = "/data/product/{pathProductId}/image/{imageId}", method = RequestMethod.DELETE)
+    public String deleteImageFromProduct(HttpServletRequest request, HttpServletResponse response,
+                                         @PathVariable int pathProductId, @PathVariable int imageId) {
+        if (!authentication(request)) {
+            response.setStatus(401);
+            return null;
+        }
+
+        if(!validateProductId(pathProductId, request)){
+            response.setStatus(400);
+            return null;
+        }
+
+        if(!validateImageId(pathProductId, imageId)){
+            response.setStatus(400);
+            return null;
+        }
+
+        ImageMeta imageMeta = ImageDataService.getInstance().getImageUsingId(imageId);
+
+        //Remove from local
+        File file = new File(imageMeta.getS3BucketPath());
+        if(file.exists()){
+            file.delete();
+        }
+        else {
+            logger.info("File doesn't exist.");
+        }
+
+        //Remove from S3 bucket
+        removeFileFromBucket(file.getName());
+
+        //Remove image meta
+        ImageDataService.getInstance().deleteImageMetaById(imageId);
+
+        return "Successfully removed";
+
+
 
 
     }
 
-
-    private boolean validateProductId(int pathProductId, HttpServletRequest request) {
-
-        String attr = request.getHeader("Authorization");
-        String[] authValue = attr.split(" ");
-        String[] decodedToken = new String(Base64.getDecoder().decode(authValue[1])).split(":");
-        User user = DataManipulationService.getInstance().getUserByUsername(decodedToken[0]);
-        Product product = ProductDataService.getInstance().getProductInfo(pathProductId);
-        if (product == null) return false;
-        if (product.getId() != pathProductId) return false;
-
-        return true;
-    }
 
     public String saveImage(String base64Img, int productId, String filename, String suffix) {
         String dirPath = "img";
         File dir = new File(dirPath);
-        if(!dir.exists()) dir.mkdir();
-        logger.info("dir status after mkdir: "+dir.exists());
+        if (!dir.exists()) dir.mkdir();
+        logger.info("dir status after mkdir: " + dir.exists());
 
         File file = new File(dir + "/product-" + productId + "-img-" + (int) (Math.random() * 10000) + "-" + filename + "." + suffix);
 
@@ -136,13 +248,25 @@ public class ProductImageController {
         return file.getAbsolutePath();
     }
 
-    public static void transferFile(File f){
+
+    //S3 manipulation
+    public static void transferFile(File f) {
         final AmazonS3 s3 = AmazonS3ClientBuilder.defaultClient();
-        String bucketName = "this-is-lrl-bucket-01";
+        String bucketName = System.getenv("S3_NAME");
 
         s3.putObject(bucketName, f.getName(), f);
     }
 
+    public static void removeFileFromBucket(String fileName){
+        final AmazonS3 s3 = AmazonS3ClientBuilder.defaultClient();
+        String bucketName = System.getenv("S3_NAME");
+
+        s3.deleteObject(bucketName, fileName);
+    }
+
+
+
+    //Authentication methods
     private boolean authentication(HttpServletRequest request) {
         String attr = request.getHeader("Authorization");
         if (attr == null) return false;
@@ -157,4 +281,31 @@ public class ProductImageController {
 
         return BCrypt.checkpw(decodedToken[1], user.getPassword());
     }
+
+    private boolean validateProductId(int pathProductId, HttpServletRequest request) {
+
+        String attr = request.getHeader("Authorization");
+        String[] authValue = attr.split(" ");
+        String[] decodedToken = new String(Base64.getDecoder().decode(authValue[1])).split(":");
+        User user = DataManipulationService.getInstance().getUserByUsername(decodedToken[0]);
+        Product product = ProductDataService.getInstance().getProductInfo(pathProductId);
+        if (product == null) return false;
+        if (product.getId() != user.getId()) return false;
+
+        return true;
+    }
+
+
+    private boolean validateImageId(int pathProductId, int imageId) {
+        //Check if the ImageId belongs to the productId
+
+        ImageMeta image = ImageDataService.getInstance().getImageUsingId(imageId);
+        Product product = ProductDataService.getInstance().getProductInfo(pathProductId);
+
+        if(image == null || product == null) return false;
+        else if(image.getProductId() != product.getId()) return false;
+        else return true;
+
+    }
+
 }
